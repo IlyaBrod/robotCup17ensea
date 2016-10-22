839a6b2d4a966e0eec00a07f525c9cc659b6c9d5
IlyaBrod <ilya.brodoline@ensea.fr>
2016-10-22 16:25:20 +0200

    Ajout des fichiers de initiaux permettant de coder.

---
+++ .build/common/mbed_mem_trace.d
@@ -0,0 +1,8 @@
+common/mbed_mem_trace.o: ../common/mbed_mem_trace.c .././mbed_config.h \
+ ../api/mbed_mem_trace.h ../api/critical.h
+
+.././mbed_config.h:
+
+../api/mbed_mem_trace.h:
+
+../api/critical.h:
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # robotCup17ensea
 Code robot, coupe de France de robotique 2017, école ENSEA.
+
+
+
+
+
+compilation par le makefile.
+
+Documentation Doxygène.
