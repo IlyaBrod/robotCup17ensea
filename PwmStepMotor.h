183783068b564c7b6f1e956cdbe07564f6363d00
huglehutt <hugot@kubuntu.hugot>
2016-11-15 16:43:32 +0100

    test branch buggué

---
+++ PwmStepMotor.h
@@ -0,0 +1,20 @@
+/**
+ * PwmStepMotor
+ *
+ * fonctinement d'un moteur pas à pas via une pwm
+ *
+ * contien le genrateur de pwm ::
+ * 		pwmm freq
+ * contien le changement de direction du moteur
+ * 		pwmm dir
+ * contien la position angulaire du moteur
+ * 		pwmm nbStep
+ * 		pwmm angle
+ */
+
+#ifndef PWMSTEPMOTOR_H
+#define PWMSTEPMOTOR_H
+
+//test
+
+#endif
