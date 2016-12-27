dea5b7721d36cf7ea178147fb0c0c1653445f162
Nicowong <nicolas.wongwanit@gmail.com>
2016-12-27 15:36:57 +0100

    pathfinding0.2

--- Pathfinding.hpp
+++ Pathfinding.hpp
@@ -1,8 +1,12 @@
 #ifndef CLASS_PATHFINDING_DEFINED
 #define CLASS_PATHFINDING_DEFINED
 
+#include "Treenode_pathfinding.hpp"
+
 class Pathfinding{
-	
+	protected :
+		Treenode_pathfinding *root ;
+		Point *path ;
 };
 
 #endif
