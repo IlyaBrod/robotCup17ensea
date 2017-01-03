af64298f93720553966e002ca4df9e87fdff4d0d
IlyaBrod <ilya.brodoline@ensea.fr>
2017-01-03 16:22:05 +0100

    Moodif Balises = objet géométrique

--- Balise.cpp
+++ Balise.cpp
@@ -1,89 +1,12 @@
 #include "Balise.hpp"
 
-
-Balise::Balise()
+Balise::Balise(int x,int y,int id)
 {
-	this->baliseRadius =RADIUS_B;
-	this->mode = 0;
-}
-
-void Balise::set_Mode(int newMode)
-{
-	if(newMode>=0 && newMode<=2) {this.mode = newMode;}
-}
-
-double Balise::tri_A_Calc(double r1,double r2,double n1, double n2)
-{
-	return 0.5*(r1*r1-r2*r2+n2-n1);
-}
-
-
-Point Balise::triangulate_From_Dist(float r1, float r2, float r3)
-{
-	Point Loc();
-
-	double a12 = triACalc(r1,r2,P1_NORM2,P2_NORM2);
-	double a13 = triACalc(r1,r3,P1_NORM2,P3_NORM2);
-
-	double Y = (a13-D13X*a12/D12X)/(D13Y-D13Y*D12Y/D12X);
-	double X = (a12-D12Y*Y)/D12X;
-
-	Loc.X = X;
-	Loc.Y = Y;
-	
-	return Loc;
-}
-
-
-Point Balise::duoangulate_1(float angle1,float angle2)
-{
-	Point Poi_Robot();
-	float r1= calc_Dist_From_Balise(angle1);
-	float r2= calc_Dist_From_Balise(angle2);
-
-
-	return Poi_Robot;
+	origine = Point(x,y);
+	ID = id;
 }
 
 
 
-float Balise::calc_Dist_From_Balise(float angle)
-{
-	float teta = 180-angle;
-	float dist = baliseRadius*sin(teta/2)/tan(angle/2)+baliseRadius*cos(teta/2) ;
-	return dist;
-}
 
-
-float Balise::get_Orientation(float db2, float db1, float angle)
-{
-	float kb2,kb1;
-	
-	kb1 = acos((-(db2*db2 - db1*db1 - B2B1*B2B1)) / (2*db1*db1))*180/PI;
-	kb2 = CORNER_B2-(180-angle-kb1);
-	
-	return 360-(180-ANGLE_B2-kb2);
-	
-}
-
-
-Point Balise::get_Point()
-{
-	Point Loc(0,0,0);
-	
-	switch(this->mode)
-	{
-		case(0):
-			break;
-		case(1):
-			
-			break;
-		case(2):
-			break;
-		else:
-			break;
-	}
-	
-	return Loc;
-}
 
--- Balise.hpp
+++ Balise.hpp
@@ -4,87 +4,32 @@
 
 
 /**
- * Représente le système de balise.\n
- * Permet le calcul de la position du robot par différntes méthodes.
+ * Represents a beacon
  */
 class Balise
 {
-
-private:
-
-	int mode;
-	
-	double baliseRadius;
-	
-	/**
-	 * Fonction qui calcule la distance robot<->balise
-	 * @param angle Angle d'observation de la balise
-	 * @return distance
-	 */
-	double calc_Dist_From_Balise(float angle);
-
-	/**
-	 * Calcule le coefficient A pour la triangulation.
-	 * @param r1 Distance balise 1
-	 * @param r2 Distance balise 2
-	 * @param n1 Norme du vecteur 1
-	 * @param n2 Norme du vecteur 2 
-	 */
-	double tri_A_Calc(double r1,double r2,double n1, double n2);
-
-
-	/**
-	 * Calcule la position du robot
-	 * à partir de la connaissance de 3 balises.
-	 * @param angle Angles d'observation des balises
-	 * @return Objet Point, position du robot
-	 */
-	Point triangulate(float angle1, float angle2, float angle3);
-
-
-	/**
-	 * Calcule la position du robot à partir de 2
-	 * balises. Avec vérification du point sur le terrain
-	 * ATTENTION : cette fonction n'est pas toujours
-	 * valide.
-	 * @param angle Angles d'observations des balises
-	 * @return objet POINT, position du robot
-	 */
-	Point duoangulate_1(float angle1,float angle2);
-
-	/**
-	 * Calcule l'angle d'orientation du robot à partir
-	 * de la balise 2 et 1.
-	 * @param db2 Distance de la balise 2
-	 * @param db1 Distance de la balise 1
-	 * @param angle Angle d'observation de la balise 2
-	 * @return Angle en degré
-	 */
-	float get_Orientation(float db2,float db1,float angle);
-
-
-public:
-
-	/**
-	 * Change le mode de calcul de la position du robot.
-	 * 0 : triangulation par distances \n
-	 * 1 : triangulation par angles \n
-	 * 2 : triangulation par 2 orientation \n
-	 */
-	void set_Mode(int newMode);
-	
-	
-	/**
-	 * Initialisaiton de la balise
-	 */
-	Balise();
+	private:
+		int ID;
+		Point origine;
+	public:
 		
-	
-	/**
-	 * Donne la position et l'orientation du robot sur le terrain.
-	 * Le calcul est fait suivant le mode choisis.
-	 * @return Point
-	 */
-	Point get_Point();
+		Balise(int x,int y,int id);
 		
+		/**
+		 * Get beacon data
+		 */
+		int get_ID();
+		int get_X();
+		int get_Y();
+		
+		/**
+		 * Set beacon location
+		 */
+		void setX(int x);
+		void setY(int y);
+		
+		
+		
+
+
 };
