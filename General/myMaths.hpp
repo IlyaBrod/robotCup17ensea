088ee0c97c373bba358b69bcfdacec3e57e79a5f
IlyaBrod <ilya.brodoline@ensea.fr>
2017-01-07 20:39:28 +0100

    Ajout commentaires + fonctions min et max dans myMaths.hpp

--- General/myMaths.hpp
+++ General/myMaths.hpp
@@ -1,5 +1,44 @@
+/**
+ * Return the maximum between 2 elements
+ * @param a
+ * @param b
+ * @return the max
+ */
+template<typename T> T min(T a,Tb)
+{
+	if(a>b)
+	{
+		return b;
+	}
+	else
+	{
+		return a;
+	}
+}
 
+/**
+ * Return the minimum between 2 elements
+ * @param a
+ * @param b
+ * @return the min
+ */
+template<typename T> T max(T a,T b)
+{
+	if(a>b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
 
+/**
+ * Return absolute value of an element
+ * @param value
+ * @return |value|
+ */
 template<typename T> T abs(T value)
 {
 	if(value<0)
@@ -13,6 +52,12 @@ template<typename T> T abs(T value)
 	
 }
 
+/**
+ * Return a power of a value : value^n
+ * @param value The value to evaluate
+ * @param n Exposant
+ * @return result
+ */
 template<typename T> float pwr(T value,int n)
 {
 	float res=1;
@@ -32,6 +77,12 @@ template<typename T> float pwr(T value,int n)
 	}
 }
 
+/***
+ * Return the square root of value
+ * @param value
+ * @param n=50 Precision of the result
+ * @return result
+ */
 template<typename T> float sqrt(T value,int n=50)
 {
 	T res=value/2;
