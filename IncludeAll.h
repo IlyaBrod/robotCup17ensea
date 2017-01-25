9d50f97ae42cc46d4015f1935f482215ee7cdff2
IlyaBrod <ilya.brodoline@ensea.fr>
2017-01-25 17:18:15 +0100

    Update main avec lidar

--- IncludeAll.h
+++ IncludeAll.h
@@ -5,7 +5,7 @@
 #include "mbed.h"
 #include "General/Point.hpp"
 #include "General/GeneralItem.h"
-#include "Lidar/Sensor/PwmStepMotor.h"
+#include "Lidar/Lidar.hpp"
 
 
 
--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@
 
 DigitalOut myled(LED1);
 //Point p(5,6,10);
-PwmStepMotor myPwm2(PB_4);
-PwmOut myPwm(PB_10);
+Lidar balise();
+//PwmOut myPwm(PB_10);
 
 int main() {
 	GeneralItem::initGeneralItem();
-	myPwm2.start();
-	myPwm.period_us(20);
-	myPwm.write(0.5);
+	//myPwm2.start();
+	//myPwm.period_us(20);
+	//myPwm.write(0.5);
     while(1) {
         myled = 1;
         wait(1);
