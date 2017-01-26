6fb692d9caac78d8fb36eb129c6fe8a121365df3
IlyaBrod <ilya.brodoline@ensea.fr>
2017-01-26 18:54:24 +0100

    Mise au propre des fichiers

--- GeneralFunctions.cpp
+++
@@ -1,9 +0,0 @@
-#include "GeneralFunctions.hpp"
-
-const float& min (const float& a, const float& b){
-  return !(b<a)?a:b;     // or: return !comp(b,a)?a:b; for version (2)
-}
-
-const float& max (const float& a, const float& b){
-  return (a<b)?b:a;     // or: return comp(a,b)?b:a; for version (2)
-}
